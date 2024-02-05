20f19c7b47d94024bdf2c1f9fd4dd122835d6a68
xDeFc0nx <nehartale8@gmail.com>
2024-02-05 22:48:53 +0100

    Feat: Basic logger

--- testbed/src/main.c
+++ testbed/src/main.c
@@ -1,6 +1,13 @@
-#include <test.h>
+#include "../../engine/src/core/logger.h"
 
 int main(void) {
-  print_int(42);
+
+  KFATAL("test message: %f", 3.44f);
+  KERROR("test message: %f", 3.44f);
+  KWARN("test message: %f", 3.44f);
+  KINFO("test message: %f", 3.44f);
+  KDEBUG("test message: %f", 3.44f);
+  KTRACE("test message: %f", 3.44f);
+
   return 0;
 }
